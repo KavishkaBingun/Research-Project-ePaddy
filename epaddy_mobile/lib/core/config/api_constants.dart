044e2920c92ed1b1043592f00047c39a308e1433
Athapaththu <sakila.atapattu@gmail.com>
2025-05-27 00:05:03 +0530

    add config and model and repo files

---
+++ epaddy_mobile/lib/core/config/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static const String baseUrl = "https://a45d-212-104-231-131.ngrok-free.app";
+
+  // ML Model Endpoints
+  static const String cropRecommend = "$baseUrl/corp_recommend";
+  static const String leafDisease = "$baseUrl/leaf_disease";
+  static const String pestPredict = "$baseUrl/pest_predict";
+  static const String paddyPhaseClassifier = "$baseUrl/paddy_phase_classifier";
+  static const String riceDisease = "$baseUrl/rice_disease";
+}
